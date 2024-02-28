62713c9af5cf7d5f8557c731eb2f54b964e981d7
chao_wang <>
2024-02-28 15:28:04 +0800

    feat:css样式重置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import './utils/vant'
+import 'normalize.css'
+import './style/index.less'
 import App from './App.vue'
 
 createApp(App).mount('#app')
